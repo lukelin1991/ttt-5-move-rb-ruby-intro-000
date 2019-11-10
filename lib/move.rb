2d76084e7391005c231b59b4b6441426e3aec965
Luke Lin <lukelin1991@yahoo.com>
2019-11-10 22:54:08 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 def input_to_index(user_input)
   user_input = gets.strip
-  puts "#{user_input}"
+  puts "#{user_input.to_i}" - 1
 end
 
 input_to_index(5)
